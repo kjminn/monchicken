62288a84ac04df51e87c177969c01511791972e6
Kyoungju Minn <kjminn2001@gmail.com>
2016-04-12 19:50:23 +0900

    #4, #5 Add SplashActivity, Activity Page Transition

---
+++ app/src/main/java/kr/hs/emirim/progh2/monchicken/SplashActivity.java
@@ -0,0 +1,32 @@
+package kr.hs.emirim.progh2.monchicken;
+
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0,R.anim.zoom_exit);
+                finish();
+            }
+        }, 3000);
+    }
+
+
+}
